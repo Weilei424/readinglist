4112c5f76a620592f381c09ba0794137c9c05152
Zhenxu Wang <mason123wp@gmail.com>
2023-05-11 01:59:34 -0400

    added redirect method

--- src/main/java/com/masonw/readinglist/ReadingListApplication.java
+++ src/main/java/com/masonw/readinglist/ReadingListApplication.java
@@ -2,7 +2,12 @@ package com.masonw.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+@RequestMapping("/")
+@RestController
 @SpringBootApplication
 public class ReadingListApplication {
 
@@ -10,4 +15,8 @@ public class ReadingListApplication {
 		SpringApplication.run(ReadingListApplication.class, args);
 	}
 
+	@GetMapping
+	RedirectView home() {
+		return new RedirectView("/readingList/{reader}");
+	}
 }
