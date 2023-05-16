c7a690851cf563c920d5ced581ae4cdd817ab7e0
Zhenxu Wang <mason123wp@gmail.com>
2023-05-15 22:30:08 -0400

    finally fixed SecurityConfig.java

--- src/main/java/com/masonw/readinglist/ReaderRepository.java
+++ src/main/java/com/masonw/readinglist/ReaderRepository.java
@@ -3,4 +3,5 @@ package com.masonw.readinglist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReaderRepository extends JpaRepository<Reader, String> {
+
 }
--- src/main/java/com/masonw/readinglist/SecurityConfig.java
+++ src/main/java/com/masonw/readinglist/SecurityConfig.java
@@ -1,50 +1,46 @@
 package com.masonw.readinglist;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     private ReaderRepository readerRepository;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").access("hasRole('READER')")
-                .antMatchers("/**").permitAll()
+                .requestMatchers("/").access("hasRole('READER')")
+                .requestMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error=true");
+        return http.build();
     }
-
-    @Override
-    protected void configure(
-            AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(new UserDetailsService() {
-                    @Override
-                    public UserDetails loadUserByUsername(String username)
-                            throws UsernameNotFoundException {
-                        UserDetails userDetails = readerRepository.findOne(username);
-                        if (userDetails != null) {
-                            return userDetails;
-                        }
-                        throw new UsernameNotFoundException("User '" + username + "' not found.");
-                    }
-                });
+    @Bean
+    public void config(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                UserDetails userDetails = readerRepository.getOne(username);
+                if (userDetails != null) {
+                    return userDetails;
+                }
+                throw new UsernameNotFoundException("User '" + username + "' not found.");
+            }
+        });
     }
-
 }
